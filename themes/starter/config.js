1a66c35408dc2906ead5b8bbe270ebd9814dd88f
a-cong-a <q961725739@gmail.com>
2025-07-01 18:21:21 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '探花万人迷+一站式', // 英雄区文字
+  STARTER_HERO_TITLE_1: '探花万人迷会员系统', // 英雄区文字
   STARTER_HERO_TITLE_2: '一站式登录 永不丢失!', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '会员登录', // 英雄区按钮
